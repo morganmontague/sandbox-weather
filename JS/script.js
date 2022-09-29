7b966848915d7f0c6204a35e648dcbf3849188a2
C. Morgan Montague <morgan.montague@student.awesomeinc.org>
2022-09-29 20:35:58 +0000

    Cleaned up code and tried to make it sorta pretty with bootstraps, I'd say its mid right now, if I have time later I will edit it more.

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,23 @@
 console.log('running')
-
-// declaring main div made in index, used to 'house' the rest of the html
-let mainDiv = document.getElementById('main');
-mainDiv.innerText = 'This is the parent Div'
-
-// creating a div to serve as the header, where the h1 and buttons will go
+function init () {
+  // declaring main div made in index, used to 'house' the rest of the html
+  let mainDiv = document.getElementById('main');
+  mainDiv.setAttribute('class', 'container-fliud "text-center')
+  // mainDiv.innerText = 'This is the parent Div'
+  
+  
+  // creating a div to serve as the header, where the h1 and buttons will go
+  // try for fix up by using function createElement created below
 let divHead = document.createElement('div')
 mainDiv.appendChild(divHead)
 divHead.setAttribute('id', 'headContainer')
-divHead.setAttribute('class', 'container')
+divHead.setAttribute('class', 'container text-center')
 let headArea = document.getElementById('headContainer')
-headArea.innerText='This will be the header with buttons';
+// headArea.innerText='This will be the header with buttons';
 
 // creating h1
+// try for fix up by using function createElement created below
+
 let h1Area = document.createElement('h1');
 headArea.appendChild(h1Area);
 h1Area.setAttribute('id', 'idTag');
@@ -20,11 +25,14 @@ h1Area.setAttribute('class', 'h1');
 h1Area.innerText = 'Weather App';
 
 // creating the label
+//labels are a little tricky for me so no createElement
 let inputLabel = document.createElement('label')
 headArea.appendChild(inputLabel);
 inputLabel.setAttribute('for', 'zipCode');
 inputLabel.setAttribute('id', 'zipLabel');
 inputLabel.innerText='Enter Zip Code';
+inputLabel.setAttribute('class', 'form-label')
+inputLabel.setAttribute('style', 'margin: 5px')
 
 // creating the input area for the zip code
 // try and add restrictions so it has to be a valid zip
@@ -33,107 +41,52 @@ headArea.appendChild(inputNumber)
 inputNumber.setAttribute('id', 'zipCode')
 inputNumber.setAttribute('type', 'number')
 inputNumber.setAttribute('name', 'zipCode')
+inputNumber.setAttribute('class', 'form-number')
+inputNumber.setAttribute('maxlength', 5)
+inputNumber.setAttribute('max', 99999)
+inputNumber.setAttribute('placeholder', 'Zip Code')
 
 // creating the submit input
-let inputSubmit = document.createElement('input')
+// changed to a button so I could control the innerText better
+let inputSubmit = document.createElement('button')
 headArea.appendChild(inputSubmit)
 inputSubmit.setAttribute('type', 'submit')
+inputSubmit.innerText = 'Submit'
 inputSubmit.setAttribute('id', 'zipCodeButtonSubmit')
+inputSubmit.setAttribute('class', "btn-primary")
+inputSubmit.setAttribute('style', 'margin: 5px')
+}
+
+// init() is sorta pointless here, just wrapped my beggining elements and called it init
+
+init()
+
+
+// declaring global variables to help my life in general..
+
+let button = document.getElementById('zipCodeButtonSubmit')
+
 
 let firstButton = document.getElementById('zipCodeButtonSubmit')
 
-async function apiInfo(url) {
-    try {
-      const response = await axios.get(url);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
+let mainDiv = document.getElementById('main')
 
-
-//creating the pop up
-
-// let popCreate = document.createElement('div')
-// mainDiv.appendChild(popCreate)
-// popCreate.setAttribute('id', 'popArea')
-// popCreate.innerText = 'new pop'
-
-// // adding the city section
-// let cityDiv = document.createElement('div')
-// popCreate.appendChild(cityDiv)
-// cityDiv.setAttribute('id', 'cityContainer')
-// let cityTitle = document.createElement('div')
-// cityDiv.appendChild(cityTitle)
-// cityTitle.setAttribute('id', 'cityTitleHead')
-// cityTitle.innerText = 'City'
-// let cityInfo = document.createElement('div')
-// cityDiv.appendChild(cityInfo)
-// cityInfo.setAttribute('id', 'cityName')
-
-// // adding temp
-// let tempDiv = document.createElement('div')
-// popCreate.appendChild(tempDiv)
-// tempDiv.setAttribute('id', 'tempContainer')
-// let tempTitle =document.createElement('div')
-// tempDiv.appendChild(tempTitle)
-// tempTitle.setAttribute('id', 'tempRow1')
-// tempTitle.innerText = 'Temperature'
-// let tempRow2 = document.createElement('div')
-// tempDiv.appendChild(tempRow2)
-// tempRow2.setAttribute('id', 'columnHolder')
-// let kelvinTemp = document.createElement('div')
-// tempRow2.appendChild(kelvinTemp)
-// kelvinTemp.setAttribute('id', 'kelvin')
-// let celsiusTemp = document.createElement('div')
-// tempRow2.appendChild(celsiusTemp)
-// celsiusTemp.setAttribute('id', 'celsius')
-// celsiusTemp.innerText = 'This will be celsius'
-// let fahrenTemp = document.createElement('div')
-// tempRow2.appendChild(fahrenTemp)
-// fahrenTemp.setAttribute('id', 'fahren')
-// fahrenTemp.innerText = 'This will be fahrenheit'
-
-
-
-// // adding condition
-// let conditionDiv = document.createElement('div')
-// popCreate.appendChild(conditionDiv)
-// conditionDiv.setAttribute('id', 'conditionContainer')
-// let conditionTitle = document.createElement('div')
-// conditionDiv.appendChild(conditionTitle)
-// conditionTitle.setAttribute('id', 'conditionTitleHead')
-// conditionTitle.innerText = 'Condition'
-// let conditionInfo = document.createElement('div')
-// conditionDiv.appendChild(conditionInfo)
-// conditionInfo.setAttribute('id', 'conditionalText')
-
-// // creating image area
-// let imageDiv = document.createElement('div')
-// popCreate.appendChild(imageDiv)
-// imageDiv.setAttribute('id', 'imgContainer')
-// let imgTitle = document.createElement('div')
-// imageDiv.appendChild(imgTitle)
-// imgTitle.setAttribute('id', 'otherTitle')
-// imgTitle.innerText = 'Other Information'
-// let imgInfo = document.createElement('div')
-// imageDiv.appendChild(imgInfo)
-// imgInfo.setAttribute('id', 'imgPlace')
-// imgInfo.innerText='This will be an image or other info' // need to figure out this part
-
+// this will make the new div container to hold my weather info
 function pop() {
   let popCreate = document.createElement('div')
   mainDiv.appendChild(popCreate)
   popCreate.setAttribute('id', 'popArea')
   popCreate.setAttribute('class', 'container')
-  popCreate.innerText = 'new pop'
-  }
-  pop()
+  // popCreate.innerText = 'new pop'
+}
+pop()
 
-  let poppy = document.getElementById('popArea')
+// named poppy in honor of poppy
+let poppy = document.getElementById('popArea')
 
-
+// This is an attempt to try and ease up my hard code, didn't get it perfect, wish I could have run my parameters through it
+// really liked the templete literals and parameters making it easy to "use"
 function createElement (area, element, text, clas, parent) {
   let id = `${area}id` 
   let createElement = document.createElement(element)
@@ -142,228 +95,187 @@ function createElement (area, element, text, clas, parent) {
   createElement.setAttribute('class', clas)
   createElement.innerText = `${text}`
 }
-createElement('city', 'div', 'LocationContainer', 'container bg-primary', poppy)
-let cityParent = document.getElementById('cityid')
-createElement('cityTitle', 'p', 'Location', 'row bg-sucess', cityParent)
-createElement('cityInfo', 'p', 'Will be city', 'row bg-primary', cityParent)
 
-createElement('temp', 'div', 'temperatureContainer', 'container bg-warning', poppy)
-let tempParent = document.getElementById('tempid')
-createElement('tempTitle', 'p', 'Temperature', 'row bg-sucess', tempParent)
-createElement('tempInfo', 'p', 'Temperature', 'row bg-warning', tempParent)
-let degreeParent = document.getElementById('tempInfoid')
-createElement('kelvin', 'p', 'Kelvin', 'col bg-sucess', degreeParent)
-createElement('celcius', 'p', 'celcius', 'col bg-warning', degreeParent)
-createElement('fahren', 'p', 'Fahrenheit', 'col bg-sucess', degreeParent)
+// This is sorta hard coded, couldn't figure out the looping with multiple parameters
+function btnClick () {
+  createElement('city', 'div', '', 'container rounded h4 bg-primary justify-content-center', poppy)
+  let cityParent = document.getElementById('cityid')
+  createElement('cityTitle', 'p', 'Location', 'bg-sucess text-center justify-content-center ', cityParent)
+  createElement('cityInfo', 'p', '', 'bg-primary text-center justify-content-center', cityParent)
+  
+  createElement('temp', 'div', '', 'container rounded bg-warning justify-content-center', poppy)
+  let tempParent = document.getElementById('tempid')
+  createElement('tempTitle', 'p', 'Temperature', ' row h4 bg-sucess text-center justify-content-center', tempParent)
+  createElement('tempInfo', 'div', '', ' container row bg-warning h4 justify-content-center', tempParent)
+  let degreeParent = document.getElementById('tempInfoid')
+  createElement('kelvin', 'p', '', 'col-md-auto   text-center', degreeParent)
+  createElement('celcius', 'p', '', 'col-md-auto  text-center', degreeParent)
+  createElement('fahren', 'p', '', 'col-md-auto  text-center', degreeParent)
+  
+  createElement('condition', 'div', '', 'container rounded h4 bg-primary justify-content-center', poppy)
+  let conditionParent = document.getElementById('conditionid')
+  createElement('conditionTitle', 'p', 'Condition', 'row justify-content-center', conditionParent)
+  createElement('conditionInfo', 'p', '', 'row justify-content-center', conditionParent)
+  
+  
+  createElement('other', 'div', '', 'container rounded h4 bg-warning justify-content-center', poppy)
+  let otherParent = document.getElementById('otherid')
+  createElement('otherTitle', 'p', 'Other Info', 'row bg-sucess text-center justify-content-center', otherParent)
+  createElement('otherInfo', 'img', '', 'row bg-sucess text-center justify-content-center', otherParent)
+}
 
-createElement('condition', 'div', 'conditionContainer', 'container bg-primary', poppy)
-let conditionParent = document.getElementById('conditionid')
-createElement('conditionTitle', 'p', 'Condition', 'row bg-sucess', conditionParent)
-createElement('conditionInfo', 'p', 'Condition', 'row bg-primary', conditionParent)
+// This is to reset the weather apps pop up, tried to just remove poppy, but it broke the whole thing so instead I remove each section one by one
+function reset () {
+  let resetCity = document.getElementById('cityid')
+  let resetTemp = document.getElementById('tempid')
+  let resetCondition = document.getElementById('conditionid')
+  let resetOther = document.getElementById('otherid')
+  // let zip = document.getElementById('zipCode').value  can't figure this part out
+  resetCity.remove()
+  resetTemp.remove()
+  resetCondition.remove()
+  resetOther.remove()
+  
+  button.innerText = "Submit";
+      button.setAttribute('class', 'btn-primary')
+}
 
-
-createElement('other', 'div', 'otherContainer', 'container bg-warning', poppy)
-let otherParent = document.getElementById('otherid')
-createElement('otherTitle', 'p', 'Other Info', 'row bg-sucess', otherParent)
-createElement('otherInfo', 'p', 'Image here', 'row bg-sucess', otherParent)
-
-
-
+// getting ready for the api call, declaring the variables used in the url, zip equals the value that the user inputs
 let country = 'us'
 let zip = document.getElementById('zipCode').value
 let key  ='61243455494583beaa9d02e13c7f29e3'
 
+// button click event, runs pop(needed to use it earlier for some reason), btnClick and zipWeather
+// also has an if else statement to allow for resets
+// need to figure out how to stop the sections from loading on a an error catch
+
+
 firstButton.addEventListener('click', () => {
-  async function zipWeather(url) {
-    
-    try {
-      const response = await axios.get(url);
-      console.log(response);
-      let k = response.data.main.feels_like
-      k = Math.round(k)
-      let c = Math.round(k - 273.15)
-      let f = Math.round(c*(9/5) + 32)
-      conditionInfoid.innerText = response.data.weather[0].description
-      kelvinid.innerText = k + ' K'
-      celciusid.innerText = c + ' C'
-      fahrenid.innerText = f + ' F'
-      cityInfoid.innerText = response.data.name
-      otherInfoid.innerText = response.data.weather[0].icon
-      
-    } catch (error) {
-      console.error(error);
+  if (button.innerText === "Submit") {
+    async function zipWeather(url) {
+      pop()
+      btnClick()
+      try {
+        const response = await axios.get(url);
+        // console.log(response);
+        let k = response.data.main.feels_like
+        k = Math.round(k)
+        let c = Math.round(k - 273.15)
+        let f = Math.round(c*(9/5) + 32)
+        conditionInfoid.innerText = response.data.weather[0].description
+        kelvinid.innerText = k + ' K'
+        celciusid.innerText = c + ' C'
+        fahrenid.innerText = f + ' F'
+        cityInfoid.innerText = response.data.name
+        let image = document.getElementById('otherInfoid')
+        // console.log(image)
+        let icon = response.data.weather[0].icon
+        let src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+        image.setAttribute('src', src)
+        image.setAttribute('class', 'rounded mx-auto d-block')
+        image.setAttribute('alt', response.data.weather[0].description)
+        
+        
+      } catch (error) {
+        reset()
+        alert('You fool');
+      }
     }
-  }
-  let zip = document.getElementById('zipCode').value
-
-  zipWeather(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${key}`)
-
-})
-
-// let cityDiv = document.createElement('div')
-// popCreate.appendChild(cityDiv)
-// cityDiv.setAttribute('id', 'cityContainer')
-// let cityTitle = document.createElement('div')
-// cityDiv.appendChild(cityTitle)
-// cityTitle.setAttribute('id', 'cityTitleHead')
-// cityTitle.innerText = 'City'
-// let cityInfo = document.createElement('div')
-// cityDiv.appendChild(cityInfo)
-// cityInfo.setAttribute('id', 'cityName')
-
-
-// function test (testWord, testWord2) {
-//   console.log(`The blind ${testWord} is going to ${testWord2}`)
-// }
-// test ('dog', 'jump')
-
-// function testElem (element, parent, id) {
-//   let newDiv = document.createElement(element)
-//   newDiv.setAttribute('id', id)
-//   parent.appendChild(newDiv)
-// }
-// testElem('div', mainDiv, 'test')
-
-// function template (inside) {
-// let insidePlace = `${inside}how`
-// console.log(insidePlace)
-// }
-// template('hi')
-
-
-// // function create2Row (element, parent, id, name){
-// //   let nameHolder = `${name}Container`
-// //   console.log(nameHolder)
-// //   let idHolder = `${id}tag`
-// //   console.log(idHolder)
-// //   console.log(element)
-// //   console.log(parent)
-// // }
-// // create2Row('div', mainDiv, 'city', 'city')
-
-// function trying (name, element, parent, area){ 
-//   let id = `${name}id`
-//   name = document.createElement(`${element}`)
-//   parent.appendChild(name)
-//   name.setAttribute('id', id)
-//   name.setAttribute('class', 'container')
-
-
-//   let innerParent = document.getElementById(id)
-
-
-//   let innerTemp = document.createElement(element)
-//   let tempId = `temp${area}`
-//   // innerParent.appendChild(innerTemp)
-//   innerTemp.setAttribute('id', tempId)
-//   innerTemp.setAttribute('class', 'row')
-
-
-  
-//   // console.log(innerParent)
-//   let innerTitle = document.createElement(element)
-//   let titleId = `title${area}`
-//   innerParent.appendChild(innerTitle)
-//   innerTitle.setAttribute('id', titleId)
-//   innerTitle.setAttribute('class', 'row')
-
-
-//   let innerInfo = document.createElement(element)
-//   let infoId = `info${area}`
-//   innerParent.appendChild(innerInfo)
-//   innerInfo.setAttribute('id', infoId)
-//   innerInfo.setAttribute('class', 'row')
-
-
-// }
-// trying('cityContainer', 'div', mainDiv, 'city')
-// trying('tempContainer', 'div', mainDiv, 'temp')
-// trying('conditionContainer', 'div', mainDiv, 'condition')
-// trying('imageContainer', 'div', mainDiv, 'img')
-
-
-  // adding the city section
-
-let state = {
-  area:
-  {cityContainer:'cityContainer',
-  cityTitle:'cityTitle',
-  cityInfo: 'cityInfo',
-
-  tempContainer: 'tempContainer',
-  tempTitle: 'tempTitle',
-  tempInfo: 'tempInfo',
-
-  conditionContainer: 'condiContainer',
-  conditionTitle: 'condiTitle',
-  conditionInfor: 'condiInfo',
-
-  otherContainer:'otherContainer',
-  otherTitle:'otherTitle',
-  otherInfo:'otherInfo',},
-
-
-
-  element: {
-  cityContainer:'div',
-  cityTitle:'p',
-  cityInfo: 'p',
-
-  tempContainer: 'div',
-  tempTitle: 'p',
-  tempInfo: 'p',
-
-  conditionContainer: 'div',
-  conditionTitle: 'p',
-  conditionInfor: 'p',
-
-  otherContainer:'div',
-  otherTitle:'p',
-  otherInfo:'p',
-  },
-
-
-
-  text: {
-  cityContainer:'',
-  cityTitle:'City',
-  cityInfo: 'place',
-
-  tempContainer: '',
-  tempTitle: 'Temperature',
-  tempInfo: 'place',
-
-  conditionContainer: '',
-  conditionTitle: 'Conditions',
-  conditionInfor: 'meh',
-
-  otherContainer:'',
-  otherTitle:'Other Info',
-  otherInfo:'boo',
-  },
-
-  
-  
-  clas: {
-    cityContainer:'container',
-    cityTitle:'col',
-    cityInfo: 'col',
+    let zip = document.getElementById('zipCode').value
     
-    tempContainer: 'container',
-    tempTitle: 'col',
-    tempInfo: 'col',
+    zipWeather(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${key}`)
+
+    button.innerText = "Reset";
+    button.setAttribute('class', 'bg-warning')
     
-  conditionContainer: 'container',
-  conditionTitle: 'col',
-  conditionInfor: 'col',
+  } else {
+    (button.innerText=== 'Reset'),
+    reset()
+      button.innerText = "Submit";
+      button.setAttribute('class', 'btn-primary')
+      button.setAttribute('placeholder', 'Ukraine')
+    }
+    
+    
+  })
   
-  otherContainer:'container',
-  otherTitle:'col',
-  otherInfo:'col',
-  }
-}
+  // the object state was going to be used if I figured out the loop with multiple paramerters
+  
+//   let state = {
+//     area:
+//     {cityContainer:'cityContainer',
+//     cityTitle:'cityTitle',
+//     cityInfo: 'cityInfo',
+    
+//     tempContainer: 'tempContainer',
+//     tempTitle: 'tempTitle',
+//     tempInfo: 'tempInfo',
+    
+//     conditionContainer: 'condiContainer',
+//     conditionTitle: 'condiTitle',
+//     conditionInfor: 'condiInfo',
+    
+//     otherContainer:'otherContainer',
+//     otherTitle:'otherTitle',
+//     otherInfo:'otherInfo',},
+    
+    
+    
+//     element: {
+//       cityContainer:'div',
+//       cityTitle:'p',
+//       cityInfo: 'p',
+      
+//       tempContainer: 'div',
+//       tempTitle: 'p',
+//       tempInfo: 'p',
+      
+//       conditionContainer: 'div',
+//       conditionTitle: 'p',
+//       conditionInfor: 'p',
+      
+//       otherContainer:'div',
+//       otherTitle:'p',
+//       otherInfo:'p',
+//     },
+    
+    
+    
+//     text: {
+//       cityContainer:'',
+//   cityTitle:'City',
+//   cityInfo: 'place',
+  
+//   tempContainer: '',
+//   tempTitle: 'Temperature',
+//   tempInfo: 'place',
+  
+//   conditionContainer: '',
+//   conditionTitle: 'Conditions',
+//   conditionInfor: 'meh',
+  
+//   otherContainer:'',
+//   otherTitle:'Other Info',
+//   otherInfo:'boo',
+// },
 
 
 
-
-
+// clas: {
+//   cityContainer:'container',
+//   cityTitle:'col',
+//   cityInfo: 'col',
+  
+//   tempContainer: 'container',
+//   tempTitle: 'col',
+//   tempInfo: 'col',
+  
+//   conditionContainer: 'container',
+//   conditionTitle: 'col',
+//   conditionInfor: 'col',
+  
+//   otherContainer:'container',
+//   otherTitle:'col',
+//   otherInfo:'col',
+// }
+// }
